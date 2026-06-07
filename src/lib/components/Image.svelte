e59e6243e4cd34c41df6a62e9cffc965feb6b006
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-06-07 14:02:26 -0600

    Add Image.svelte component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+    type props = {
+      class?: string,
+      src: string,
+      alt: string,
+      loadLazily: boolean
+    }
+    
+    let {
+      class: klass = "",
+      src,
+      alt = "",
+      loadLazily = true
+    }: props = $props();
+    
+</script>
+
+<img
+    loading={loadLazily ? "lazy":"eager"}
+	{src}
+	{alt}
+	class="h-full w-full object-contain {klass}"
+/>
