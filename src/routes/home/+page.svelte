46a23250a3132308d4c94442025f5021bb8377eb
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-06-07 14:02:43 -0600

    Add home and login pages

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { resolve } from "$app/paths";
+</script>
+
+<h1>
+    en la pagina home
+</h1>
+
+<a href={resolve("/")}> regresar </a>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { resolve } from "$app/paths";
+
+    
+</script>
+
+<h1>
+    en la pagina login
+</h1>
+
+<a href={resolve("/")}> regresar </a>
