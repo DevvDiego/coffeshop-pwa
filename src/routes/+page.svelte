7665859f86ae8229606d77dca07c0e837b808b73
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-06-07 14:01:47 -0600

    Add initial loading screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Image from '$lib/components/Image.svelte';
+	import happy_cup from '$lib/assets/svgs/happy_cup.svg';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	import { auth } from '$lib/auth.svelte';
+	
+	// onMount(()=>{
+ //        setTimeout(()=>{
+ //            goto(resolve("/home"))
+ //        }, 4000);
+	// });
+	
+</script>
+
+<div
+	class="
+    sticky top-0 left-0
+    h-lvh w-lvw
+    bg-yellow-100
+"
+>
+	<div
+		class="
+        absolute top-6/12 left-1/2 flex w-full
+        -translate-x-1/2
+        -translate-y-1/2
+        flex-col flex-nowrap items-center
+        "
+	>
+	    <!-- added pr-2 to accomodate icon -->
+		<div class="w-2/8 pr-2 animate-bounce"> 
+			<Image loadLazily={false} src={happy_cup} alt="Logo de carga" />
+		</div>
+		<!-- <h1 class="mt-3 text-center text-xl">
+			Cafeteria Lirios
+		</h1> -->
+	</div>	
+</div>
