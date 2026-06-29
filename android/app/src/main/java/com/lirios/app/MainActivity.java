21d6c8379d9b875f33b73da3997065bfcb09ee71
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-06-28 19:11:39 -0600

    Add android config folder and capacitor.config.ts

---
+++ android/app/src/main/java/com/lirios/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lirios.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lirios.app',
+  appName: 'Lirios',
+  webDir: 'build'
+};
+
+export default config;
